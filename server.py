68d1ec81d27e0c8b64a507069c9e41c928f512d4
lingz <lz781@nyu.edu>
2014-08-07 10:15:32 -0700

    Password auth added

--- server.py
+++ server.py
@@ -3,8 +3,39 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 import time
+import base64
 
 datastore = {"id": ""}
+config = {}
+with open("config.txt") as f:
+  for line in f:
+    opts = line.split("=")
+    config[opts[0]] = opts[1].rstrip()
+
+def require_basic_auth(handler_class):
+    def wrap_execute(handler_execute):
+        def require_basic_auth(handler, kwargs):
+            auth_header = handler.request.headers.get('Authorization')
+            if auth_header is None or not auth_header.startswith('Basic '):
+                handler.set_status(401)
+                handler.set_header('WWW-Authenticate', 'Basic realm=Restricted')
+                handler._transforms = []
+                handler.finish()
+                return False
+            auth_decoded = base64.decodestring(auth_header[6:])
+            username, password = auth_decoded.split(':', 2)
+            authenticated = username == config['user'] and config['pass'] == password
+            kwargs['isAuthenticated'] = str(authenticated)
+            if not authenticated:
+                handler.set_status(401)
+            return authenticated
+        def _execute(self, transforms, *args, **kwargs):
+            require_basic_auth(self, kwargs)
+            return handler_execute(self, transforms, *args, **kwargs)
+        return _execute
+
+    handler_class._execute = wrap_execute(handler_class._execute)
+    return handler_class
 
 class ServerHandler(tornado.web.RequestHandler):
   def initialize(self, datastore):
@@ -19,20 +50,21 @@ class ServerHandler(tornado.web.RequestHandler):
     self.datastore["id"] = newId
     self.write("Success")
 
+@require_basic_auth
 class MyFileHandler(tornado.web.StaticFileHandler):
   def initialize(self, path):
     self.dirname, self.filename = os.path.split(path)
     super(MyFileHandler, self).initialize(self.dirname)
 
-  def get(self, path=None, include_body=True):
-    # Ignore 'path'.
-    super(MyFileHandler, self).get(self.filename, include_body)
+  def get(self, isAuthenticated, path=None, include_body=True):
+    if isAuthenticated:
+      super(MyFileHandler, self).get(self.filename, include_body)
 
 class IndexHandler(tornado.web.RequestHandler):
   def initialize(self, datastore):
     self.datastore = datastore
 
-  def get(self):
+  def get(self, **kwargs):
     self.render("index.html", idNum=self.datastore["id"])
 
 
